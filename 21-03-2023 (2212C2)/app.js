acef2224664b83ba73c34b8fd8fd29876dd0a8bf
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-03-21 13:05:55 +0500

    21-03-2023

---
+++ 21-03-2023 (2212C2)/app.js
@@ -0,0 +1,94 @@
+// let empSalary=[[14350,10500,1500,26350],[34350,4050,1000,39400],[6150,4500,3250,13900],[4920,4500,2250,11670],[12300,9000,2000,23300]]
+
+// document.write("Employee Salary Details: <br><br>")
+// for(let i=0;i<empSalary.length;i++){
+//     for(let j=0;j<empSalary[i].length;j++){
+        
+//         document.write(empSalary[i][j] + " ")
+//     }
+//     document.write("<br>")
+// }
+
+// var nums=[0,1,2,3,4]
+
+// for(let i=1;i<nums.length;i++){
+//     document.write(nums[i]+"<br>")
+// }
+
+// var nums=[5,1,4,0,9,6]
+
+// nums.sort().reverse()
+
+// document.write(nums)
+
+// let nums=[1,2,3,4,5]
+
+// nums.reverse()
+
+// document.write(nums)
+
+// const arr1 = ["Cecilie", "Lone"]
+// const arr2 = ["Emil", "Tobias", "Linus"]
+// const arr3 = ["Robin", "Morgan"]
+
+// let myChildren = arr1.concat(arr2, arr3)
+
+// document.write("<h1>"+myChildren+"</h1>")
+
+// let animals=["Cats","Dogs","Lions","Cats","Dogs","Cats"]
+
+// // document.write(animals.indexOf("Cats"))
+// document.write(animals.lastIndexOf("Cats"))
+
+// let sentence="I Love Pakistan"
+
+
+// document.write(sentence)
+
+// document.write(sentence.replace("I","We"))
+
+// let sentence="I love Imran Khan"
+
+// document.write("Original Sentence: "+sentence+"<br>")
+
+// document.write("After Replacing: "+sentence.replace("Imran Khan","Pakistan"))
+
+// document.write("After Replacing: "+sentence.replaceAll("I","We"))
+// document.write(sentence[2])
+
+// document.write(sentence.length)
+
+// let userName=prompt("Please enter your name")
+// for(let i=0;i<userName.length;i++){
+//     document.write(userName[i]+"<br>")
+// }
+
+// let userName=prompt("Please enter your name")
+// for(let i in userName){
+//     document.write(userName[i]+"<br>")
+// }
+
+
+// let sentence="I love Imran Khan"
+
+// document.write("Original Sentence: "+sentence+"<br>")
+// document.write("After UpperCase: "+sentence.toUpperCase())
+
+// document.write("After UpperCase: "+sentence.toLowerCase())
+
+
+// document.write(sentence.slice(2,5))
+// let part = sentence.substring(7, 13)
+// document.write(part)
+
+
+// let alphabet=prompt("Please enter a letter")
+
+// if(alphabet.toLowerCase()==="a" || alphabet.toLowerCase()==="e" || alphabet.toLowerCase()==="i" || alphabet.toLowerCase()==="o" || alphabet.toLowerCase()==="u"){
+//     document.write("Vowel")
+// }
+// else{
+//     document.write("Consonant")
+// }
+
+
